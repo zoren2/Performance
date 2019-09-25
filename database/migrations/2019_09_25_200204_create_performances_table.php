9948a087f20dbe6ef7f2a804934647c8c2933d5b
Jack Tsang <cic_tian2@yahoo.com>
2019-09-25 14:34:33 -0700

    Added Performance Eloquent model to the application

---
+++ database/migrations/2019_09_25_200204_create_performances_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePerformancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('performances', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('sample_id');
+            $table->float('hard_drive_total');
+            $table->float('hard_drive_free');
+            $table->float('cpu_usage');
+            $table->float('vmem_total');
+            $table->float('vmem_free');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('performances');
+    }
+}
