6c15125485fcde52d6f19c7ec3323685669c215f
Jack Tsang <cic_tian2@yahoo.com>
2019-09-30 17:14:27 -0700

    Added User to Performance relationship
    User has performance

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function performances()
+    {
+        return $this->hasMany(Performance::class);
+    }
 }
